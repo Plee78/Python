e38f36a6041bdb89fc6424398b2dd7fcb155172f
Patrick Lee <125105019+Plee78@users.noreply.github.com>
2023-05-15 17:51:43 -0500

    Add files via upload

---
+++ Section 3 Introduction to Strings.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'hello'
+
+
+# In[2]:
+
+
+"world"
+
+
+# In[3]:
+
+
+'this is also a string'
+
+
+# In[4]:
+
+
+"I'm going on a run"
+
+
+# In[5]:
+
+
+print("hello")
+
+
+# In[7]:
+
+
+"hello world one"
+"hello world two"
+
+
+# In[8]:
+
+
+print("hello world one")
+print("hello world two")
+
+
+# In[10]:
+
+
+print('hello \n world')
+
+
+# In[11]:
+
+
+print('hello \t world')
+
+
+# In[12]:
+
+
+len("hello")
+
+
+# In[13]:
+
+
+len("I am")
+
+
+# In[ ]:
+
+
+
+
