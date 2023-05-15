48db157475c19468423be4885acfc00d3977e873
Patrick Lee <125105019+Plee78@users.noreply.github.com>
2023-05-15 17:40:59 -0500

    Add files via upload

---
+++ Section 3 Variable Assignments.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 5
+
+
+# In[2]:
+
+
+a
+
+
+# In[3]:
+
+
+a = 10
+
+
+# In[4]:
+
+
+a
+
+
+# In[5]:
+
+
+a + a
+
+
+# In[6]:
+
+
+a = a + a
+
+
+# In[7]:
+
+
+a
+
+
+# In[8]:
+
+
+type(a)
+
+
+# In[9]:
+
+
+a = 30.1
+
+
+# In[10]:
+
+
+type(a)
+
+
+# In[11]:
+
+
+my_income = 100
+
+
+# In[12]:
+
+
+tax_rate = 0.1
+
+
+# In[13]:
+
+
+my_taxes = my_income * tax_rate
+
+
+# In[14]:
+
+
+my_taxes
+
+
+# In[ ]:
+
+
+
+
